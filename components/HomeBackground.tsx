fc33651fd5ff984fe5fcfc0c5ff7979127c6f3e9
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-02-15 14:44:59 +0300

    HomeBackground component

---
+++ components/HomeBackground.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeBackground = () => {
+  return (
+    <View>
+      <Text>HomeBackground</Text>
+    </View>
+  )
+}
+
+export default HomeBackground
+
+const styles = StyleSheet.create({})
