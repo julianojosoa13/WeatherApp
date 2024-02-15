899e2823ddc80d7a7e568524d0eabcf585c6cf1a
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-02-15 23:15:10 +0300

    Smoke effect

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,12 +1,16 @@
 import { ImageBackground, StyleSheet, Image, Text, View, useWindowDimensions, ScaledSize } from 'react-native'
 import React from 'react'
-import { Canvas, LinearGradient, Rect, vec } from '@shopify/react-native-skia'
+import { Canvas, Line, LinearGradient, Rect, vec } from '@shopify/react-native-skia'
+import useApplicationDimensions from '../hooks/useApplicationDimensions'
 
 const HomeBackground = () => {
-  const dimensions = useWindowDimensions()
+  const dimensions = useApplicationDimensions()
   const {width, height} = dimensions
 
   const myStyles = styles(dimensions)
+
+  const smokeHeight = height * 0.6
+  const smokeOffsetY = height * 0.4
 
   return (
     <>
@@ -20,6 +24,19 @@ const HomeBackground = () => {
             </Rect>
         </Canvas>
         <ImageBackground source={require("../assets/home/Background.png")} resizeMode='cover' style={{height:"100%"}}>
+            <Canvas style={{height: smokeHeight, ...StyleSheet.absoluteFillObject, top: smokeOffsetY}}>
+                <Rect x={0} y={0} width={width} height={smokeHeight}>
+                    
+                    <LinearGradient 
+                        start={vec(width/2,0)}
+                        end={vec(width/2,smokeHeight)}
+                        colors={["rgba(58,63,84,0)", "rgba(58,63,84,1)"]}
+                        positions={[-0.02,0.54]}
+                    />
+                    
+                </Rect>
+            </Canvas>
+            
             <Image 
                 source={require("../assets/home/House.png")}
                 resizeMode='cover' 
