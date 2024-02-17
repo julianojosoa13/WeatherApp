1fcefa87289bbc16341cabceb12a9af4cef324ae
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-02-17 21:17:01 +0300

    ArcComponent stroke

--- components/tabbar/elements/ArcComponent.tsx
+++ components/tabbar/elements/ArcComponent.tsx
@@ -9,6 +9,7 @@ interface ArcComponentProps {
 
 const ArcComponent = ({height,width}: ArcComponentProps) => {
   const arcPath = `M 0 0 Q ${width/2} ${height/2} ${width} 0 L ${width} ${height} L 0 ${height} Z`
+  const arcBorder = `M 0 0 Q ${width/2} ${height/2} ${width} 0`
   return (
     <Canvas
       style={{
@@ -23,6 +24,14 @@ const ArcComponent = ({height,width}: ArcComponentProps) => {
           end={vec(width/2,height)}
           colors={["rgba(58,58,106,1)","rgba(37,36,76,1)"]}
         />
+      </Path>
+      <Path
+        path={arcBorder}
+        style={"stroke"}
+        strokeWidth={0.5}
+        color={"rgba(117, 130, 244, 0.5)"}
+      >
+
       </Path>
 
     </Canvas>
