284fe78caefba079167d5a6c479ea51c9710b466
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-02-18 14:13:32 +0300

    Displaying the current weather

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Canvas, Rect, LinearGradient, vec} from "@shopify/react-native-skia";
 import HomeBackground from './components/HomeBackground';
 import WeatherTabBar from './components/tabbar/WeatherTabBar';
+import WeatherInfo from './components/section/WeatherInfo';
+import { currentWeather } from './data/CurrentWeather';
 
 export default function App() {
   return (
     <>
       <HomeBackground />
+      <WeatherInfo weather={currentWeather}/>
       <WeatherTabBar />
       <StatusBar style='light'/>
     </>
---
+++ components/section/WeatherInfo.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Weather } from '../../models/Weather'
+import { DEGREE_SYMBOL } from '../../utils/Constants'
+
+interface WeatherInfoProps {
+    weather: Weather
+}
+const WeatherInfo = ({weather}:WeatherInfoProps) => {
+  const {city, high, low, temperature, condition} = weather
+  return (
+    <View>
+      <Text style={styles.cityText}>{city}</Text>
+      <Text style={styles.temperatureText}>{temperature}</Text>
+      <Text style={styles.conditionText}>{condition}</Text>
+      <Text style={styles.minMaxText}>H: {high}{DEGREE_SYMBOL}    L: {low}{DEGREE_SYMBOL}</Text>
+    </View>
+  )
+}
+
+export default WeatherInfo
+
+const styles = StyleSheet.create({})
