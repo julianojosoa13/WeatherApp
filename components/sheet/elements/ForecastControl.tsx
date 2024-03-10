893814cc53f0771fbab90aa1de3fb0dc3ff5e10f
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-03-10 21:02:44 +0300

    Forecast Control indicator animation

--- components/sheet/elements/ForecastControl.tsx
+++ components/sheet/elements/ForecastControl.tsx
@@ -1,13 +1,15 @@
-import { LayoutChangeEvent, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { LayoutChangeEvent, StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from 'react-native'
 import React, { useState } from 'react'
 import { Canvas, Line, LinearGradient, vec } from '@shopify/react-native-skia'
 import { ForecastType } from '../../../models/Weather'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 
 interface ForecastControlProps {
     onPress: (forecastType: ForecastType) => void,
 }
 
 const ForecastControl = ({onPress}: ForecastControlProps) => {
+  const {width} = useWindowDimensions()
   const [textWidth, setTextWidth] = useState(0)
      
   const onTextLayout = (event: LayoutChangeEvent) => {
@@ -15,23 +17,49 @@ const ForecastControl = ({onPress}: ForecastControlProps) => {
   }
   const spacingX = 32   
 
-  const strokeWidth = 3
+  const strokeWidth = 5
+
+  const segmentTranslateX = useSharedValue(0)
+
+  const AnimatedCanvas = Animated.createAnimatedComponent(Canvas)
+
+  const animatedSegmentLineStyle = useAnimatedStyle(()=> {
+    return {
+        transform: [{translateX: segmentTranslateX.value}]
+    }
+  })
+
+  const onForecastPress = (type: ForecastType) => {
+    if (type === ForecastType.Weekly) {
+        segmentTranslateX.value = withTiming(width  - textWidth - spacingX * 2, {
+            duration: 600,
+        })
+    } else {
+        segmentTranslateX.value = withTiming(0, {
+            duration: 600,
+        })
+    }
+    onPress(type)
+  }
   return (
     <>
         <View style={{flexDirection: "row", justifyContent: "space-between", paddingHorizontal: spacingX}}>
-            <TouchableOpacity onPress={() => onPress(ForecastType.Hourly)}>
+            <TouchableOpacity onPress={() => onForecastPress(ForecastType.Hourly)}>
                 <Text onLayout={onTextLayout} style={styles.forecastText}>Hourly Forecast</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => onPress(ForecastType.Weekly)}>
+            <TouchableOpacity onPress={() => onForecastPress(ForecastType.Weekly)}>
                 <Text onLayout={onTextLayout} style={styles.forecastText}>Weekly Forecast</Text>
             </TouchableOpacity>
         </View>
-        <Canvas
-            style={{
+        <AnimatedCanvas
+            style={[
+                {
                 height: strokeWidth,
                 width: textWidth,
                 marginLeft: spacingX,
-            }}
+                },
+                animatedSegmentLineStyle
+            ]}
         >
             <Line 
                 p1={vec(0,0)}
@@ -44,7 +72,7 @@ const ForecastControl = ({onPress}: ForecastControlProps) => {
                     colors={["rgba(47,112,177,0)", "rgba(147,112,177,1)","rgba(47,112,177,0)"]}
                 />
             </Line>
-        </Canvas>
+        </AnimatedCanvas>
     </>
   )
 }
