12c36ef0d93669d9094bcbde027b8e0f463521d0
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-03-11 16:06:31 +0300

    Custom Appbar for the WeatherList screen

--- screens/WeatherList.tsx
+++ screens/WeatherList.tsx
@@ -1,21 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import BackgroundGradient from '../components/BackgroundGradient'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import {Ionicons} from "@expo/vector-icons"
+import { ParamListBase, useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+
 
 const WeatherList = () => {
+  const navigation = useNavigation<StackNavigationProp<ParamListBase>>()
+  const {top} = useSafeAreaInsets()
   return (
-    <View
-        style={styles.container}
-    >
+    <>
       <BackgroundGradient />
-    </View>
+      <View
+        style={{paddingTop: top + 2, flex: 1}}
+      >
+        <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 16, paddingBottom: 8}}>
+            <View style={{flexDirection: "row", alignItems: "center"}}>
+                <Pressable
+                    onPress={() => navigation.navigate("Home")}
+                    hitSlop={20}
+                >
+                    <Ionicons 
+                        name="chevron-back-sharp"
+                        size={34}
+                        color={"rgba(235,235,245,0.6))"}
+                    />
+                </Pressable>
+                <Text
+                    style={styles.titleText}
+                >
+                    Weather
+                </Text>
+            </View>
+            <Ionicons 
+                name="ellipsis-horizontal-circle"
+                size={34}
+                color={"#FFF"}
+            />
+        </View>
+      </View>
+    </>
   )
 }
 
 export default WeatherList
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    titleText:{
+        color: "#FFF",
+        fontFamily: "SF-Semibold",
+        fontSize: 28,
+        lineHeight: 34
     }
 })
